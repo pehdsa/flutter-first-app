baa18a1ba790031f608a0452aff160168c41ccc1
InfoShop <infoshop@iMac-de-InfoShop.local>
2021-02-10 17:54:46 -0400

    mensagem

--- lib/components/list.dart
+++ lib/components/list.dart
@@ -4,12 +4,16 @@ import './stories.dart';
 class InstaList extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
+
+        var deviceSize = MediaQuery.of(context).size;
+
         return ListView.builder(
             itemCount: 5,
-            itemBuilder: (context, index) => index == 0 ? SizedBox(
-                
+            itemBuilder: (context, index) => index == 0 ? 
+              SizedBox(
+                height: deviceSize.height * 0.15,
                 child: InstaStories(),
-            ) : Column(),
+              ) : Column(),
         );
     }
 }
