e0bab78acd6bde1b45f9b2daae5c467879629068
InfoShop <infoshop@iMac-de-InfoShop.local>
2021-02-10 13:59:25 -0400

    mensagem

---
+++ lib/components/teste_stateless.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class NomeCla extends StatelessWidget {
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            child: Column(children: [],),
+        );
+    }
+}
